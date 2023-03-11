60d20b8dfb5df61156d6bad70d403eb7fec3aea7
Muhamad Riyanto <muhamad.riyanto185@gmail.com>
2023-03-11 18:47:49 -0500

    menambahkan fitur destinasi dan kuliner

--- resources/views/kuliner.blade.php
+++ resources/views/kuliner.blade.php
@@ -9,7 +9,7 @@
     <!-- icon from font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <!-- css -->
-    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     
@@ -28,26 +28,174 @@
               <a class="nav-link active" href="/">Beranda</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="destinasi">Destinasi</a>
+              <a class="nav-link" href="/destinasi">Destinasi</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="kuliner">Kuliner</a>
+              <a class="nav-link" href="/kuliner">Kuliner</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="login">Login</a>
+              <a class="nav-link" href="/login">Login</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="signup">Sign Up</a>
+              <a class="nav-link" href="/signup">Sign Up</a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  
+    <!-- Hero -->
+    <section class="d-flex align-items-center min-vh-100">
+      <div class="container">
+        <div class="row">
+          <!-- left col -->
+          
+          <!-- right col -->
+          <div class="col-md-6">
+            <div class="ms-3 ps-5 pe-5">
+              <h1 class="text-green display-2 fw-bold mt-3">Destinasi</h1>
+              <p class="mt-3 mb-5 ">Nikmati berbagai keindahan yang terkenal dengan sebutan kota pahlawan serta kunjungi berbagai tempat bersantai dengan teman dan keluarga. </p>
+              <a href="" class="btn btn-brand " style="color: #fff;">Lihat Sekarang</a>
+            </div>
+          </div>
+          
+          <div class="col-md-6">
+            <img class="img-fluid object-fit-cover rounded-4 shadow" src="{{asset('images/rasyid-maulana-yVwiHXoTrnU-unsplash.jpg')}}" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
     
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none;" class="btn btn-success">All</button>
+        <button type="button" style="background-color: #E3FBD7; color: #8E9D86; border-radius: 40px; border: none;" class="btn btn-success">Destinasi</button>
+        <button type="button" style="background-color: #E3FBD7; color: #8E9D86; border-radius: 40px; border: none;" class="btn btn-success">Kuliner</button>
+        <button type="button" style="background-color: #E3FBD7; color: #8E9D86; border-radius: 40px; border: none;" class="btn btn-success"> ...........</button>
+      </div>
+    </div>
+  </div>
 
+  <!-- gwalk -->
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- kedua -->
+
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+      <div class="col-md-3">
+        <img src="images/gwalk.jpg" class="img-fluid rounded">
+        <button type="button" style="background-color: #4AC064; border-radius: 40px; border: none; height: 26px; width: 66px; " class="btn btn-success">All</button>
+        <h3 class="" style="font-size: 30px;">G Walk Citraland Surabaya</h3>
+        <p style="font-size: 16px;">Baca Sekarang</p>
+      </div>
+    </div>
+  </div>
+  <!-- end gwalk -->
+
+
+
+
+    <!-- footer -->
+    <footer class="mt-5 pt-5 pb-5 mb-5" style="background-color: #F3FDEE;">
+      <div class="container">
+        <div class="row pt-5">
+          <div class="col-md-2">
+            <img src="https://tourism.surabaya.go.id/assets/front/images/img-logo-sparkling.png" style="width: 100px;" alt="">
+          </div>
+          <div class="col-md-8 text-center">
+            <p style="color: #4AC064;">@2018 - 2023 Wisata Surabaya</p> 
+          </div>
+          <div class="col-md-2 text-right">
+            <i class="fa-brands fa-facebook fa-2x" style="color: #4AC064;"></i>
+            <i class="fa-brands fa-instagram fa-2x ms-4" style="color: #4AC064;"></i>
+          </div>
+          <hr class="mt-5" style="color: #4AC064;">
+        </div>
+
+
+        <!-- oke -->
+        <div class="row mt-5">
+          <div class="col-md-3">
+            <h5>Find Us</h5>
+            <p>Tourism Information Center <br>
+              Jl. Gubernur Suryo 15</p>
+          </div>
+          <div class="col-md-2">
+            <h5>Contact Us</h5>
+            <p>Dinas Kebudayaan m Kepemudaan dan Olahraga serta Pariwisata Kota Surabaya</p>
+
+            <p>
+              Jl. Tunjungan No.1-3 Kelurahan <br> Genteng Kecamatan Genteng, Kota <br> Surabaya Jawa Timur, Indonesia, <br> 60275
+            </p>
+
+            <p>031-5318409</p>
+            <p>disbudporapar@surabaya.go.id</p>
+          </div>
+          
+          <div class="col-md-5">
+            <h5>Our Other Sites</h5>
+            <p>Disbudporapar Surabaya</p>
+            <p>360° Surabaya</p>
+            <p>Tiket Wisata Surabaya</p>
+            <p>Katalog Museum Surabaya</p>
+            <p>Bangga Surabaya</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- end footer -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="./assets/js/app.js"></script>
+
+    <!-- my js -->
+    <script src="js/app.js"></script>
   </body>
 </html>
